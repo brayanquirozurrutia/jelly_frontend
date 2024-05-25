50bd1a307c72a2b17076bca49507cb666463beac
BrayanQuirozUrrutia <brayanquirozurrutia@gmail.com>
2024-05-25 18:43:32 -0400

    :iphone: FEAT: Agrega diseño responsivo del header

--- src/components/commons/Header/Header.tsx
+++ src/components/commons/Header/Header.tsx
@@ -5,32 +5,29 @@ import { logoUrl } from "../../../constants/constants.ts";
 
 const Header: React.FC = () => {
     return (
-        <header className="flex items-center justify-between px-4 py-4 text-stone-950">
-            {/* Logo */}
-            <div className="flex items-center space-x-4">
-                <img src={logoUrl} alt="Logo" className="w-40 h-auto" />
-                <h1 className="text-3xl">PONER ALGUNA FRASE</h1>
+        <header className="flex flex-col xl:flex-row items-center justify-between px-4 py-4 text-stone-950 space-y-4 xl:space-y-0">
+            {/* Logo y H1 */}
+            <div className="flex items-center space-x-4 w-full xl:w-auto">
+                <img src={logoUrl} alt="Logo" className="w-24 md:w-32 lg:w-40 h-auto" />
+                <h1 className="text-lg md:text-2xl lg:text-3xl">PONER ALGUNA FRASE</h1>
             </div>
 
             {/* Elementos a la derecha */}
-            <div className="flex items-center space-x-4 relative">
+            <div className="flex flex-col md:flex-row xl:flex-row items-center justify-center md:justify-center xl:justify-between space-y-2 md:space-y-0 md:space-x-2 xl:space-y-0 w-full xl:w-auto">
                 {/* Buscador de productos */}
-                <div className="relative">
+                <div className="relative w-full md:w-64 xl:w-64">
                     <input type="text" placeholder="Buscar productos"
-                           className="pl-10 pr-3 py-1 border border-gray-600 rounded-lg focus:border-purple1 focus:outline-none transition-all duration-150 focus:ring-2 focus:ring-purple1"/>
+                           className="w-full pl-10 pr-3 py-2 border border-gray-600 rounded-lg focus:border-purple1 focus:outline-none transition-all duration-150 focus:ring-2 focus:ring-purple1"/>
                     <FontAwesomeIcon icon={faSearch}
                                      className="absolute left-3 top-1/2 transform -translate-y-1/2 text-black text-xl cursor-pointer"/>
                 </div>
 
                 {/* Inicio de sesión */}
                 <button
-                    className="px-3 py-1 bg-purple1 hover:bg-purple2 text-black font-bold rounded-lg flex items-center space-x-2">
+                    className="w-full md:w-auto xl:w-auto px-3 py-2 bg-purple1 hover:bg-purple2 text-black font-bold rounded-lg flex items-center justify-center md:justify-center xl:justify-start space-x-2">
                     <FontAwesomeIcon icon={faUser} className="text-black text-xl"/>
                     <span>Iniciar sesión</span>
                 </button>
-
-                {/* Separador vertical */}
-                <div className="border-l border-gray-400 h-6"></div>
 
                 <div className="flex items-center space-x-2">
                     {/* Icono de carrito */}
@@ -43,7 +40,6 @@ const Header: React.FC = () => {
                     <span
                         className="bg-red-500 text-white text-xs font-semibold flex items-center justify-center w-5 h-5 rounded-full">3</span>
                 </div>
-
             </div>
         </header>
     );
